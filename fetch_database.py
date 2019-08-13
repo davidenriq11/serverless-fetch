6b2202b874b97c4ff612f3b66726251902913432
david <david.enriquez@busolinea.com>
2019-08-13 17:56:15 -0500

    Serverlesize

--- fetch_database.py
+++ fetch_database.py
@@ -1,20 +1,21 @@
 import os
 import pymysql.cursors
 
-try:
-   connection = pymysql.connect(host=os.environ["LOCALHOST"],
-                             database=os.environ["DATABASE"],
-                             user=os.environ["USER"],
-                             password=os.environ["PASSWORD"],
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-   query = "select * from {}".format(os.environ["TABLE"])
-   cursor = connection .cursor()
-   cursor.execute(query)
-   records = cursor.fetchall()
-   print(records)
+def fetch_data(context, event):
+    try:
+       connection = pymysql.connect(host=os.environ["LOCALHOST"],
+                                 database=os.environ["DATABASE"],
+                                 user=os.environ["USER"],
+                                 password=os.environ["PASSWORD"],
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+       query = "select * from {}".format(os.environ["TABLE"])
+       cursor = connection .cursor()
+       cursor.execute(query)
+       records = cursor.fetchall()
+       print(records)
 
-except Error as e :
-    print ("Error while connecting to Database", e)
-finally:
-    connection.close()
+    except Error as e :
+        print ("Error while connecting to Database", e)
+    finally:
+        connection.close()
